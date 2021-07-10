6492d09130407d8f8fc8d3c597b7c51b0cb76d19
Andrii Holovchak <the.and.ho@gmail.com>
2021-07-10 19:46:11 +0300

    check env type

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import "./App.sass";
+import { useAppDispatch, useAppSelector } from "./app/hooks";
+import { changeIsMobileEnv, selectIsMobileEnv } from "./appSlice";
 
 function App() {
+  const isMobile = useAppSelector(selectIsMobileEnv);
+  const dispatch = useAppDispatch();
+
+  const handleWindowResize = useCallback(() => {
+    const windowWidth = window.innerWidth;
+
+    if (+(windowWidth < 1024) ^ +isMobile) {
+      dispatch(changeIsMobileEnv(!isMobile));
+    }
+  }, [isMobile, dispatch]);
+
+  useEffect(() => {
+    window.addEventListener("resize", handleWindowResize);
+    return () => window.removeEventListener("resize", handleWindowResize);
+  }, [handleWindowResize]);
+
+  useEffect(() => {
+    dispatch(changeIsMobileEnv(window.innerWidth < 1024));
+  }, [dispatch]);
   return (
     <div className="App">
       <h4>Hello World!</h4>
