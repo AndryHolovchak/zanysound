92807e114f425872b15067459063c6c4c708358e
Andrii Holovchak <the.and.ho@gmail.com>
2021-07-11 18:44:36 +0300

    created Track component

---
+++ src/components/Track/Track.tsx
@@ -0,0 +1,25 @@
+import classNames from "classnames";
+import React from "react";
+import { TrackModel } from "../../commonTypes/modelTypes";
+import style from "./track.module.sass";
+
+export interface TrackProps {
+  model: TrackModel;
+  className?: string;
+}
+
+const Track: React.FC<TrackProps> = ({ model, className }) => {
+  const finalClassName = classNames([style.track, className]);
+
+  return (
+    <div className={finalClassName}>
+      <img alt="cover" src={model.album.cover} className={style.track__cover} />
+      <div className={style.track__main_info}>
+        <span className={style.track__title}>{model.title}</span>
+        <span className={style.track__artist}>{model.artist.name}</span>
+      </div>
+    </div>
+  );
+};
+
+export default Track;
